c2b563d5e1a05dd9c41f1db6cbcdea8760a07691
Max Mraz <max.mraz.11@gmail.com>
2020-09-25 22:42:41 -0400

    layout for creepytown, graphics

--- data/maps/town.lua
+++
@@ -1,7 +0,0 @@
-local map = ...
-local game = map:get_game()
-
-map:register_event("on_started", function()
-  map.light_fx:set_darkness_level({180, 160, 200})
-  map.light_fx:set_darkness_level("dusk")
-end)
