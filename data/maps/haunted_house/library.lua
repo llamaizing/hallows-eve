f49b62fe55f81ac3d2627c843be3ed4748a864a8
Max Mraz <max.mraz.11@gmail.com>
2020-10-02 16:52:24 -0400

    start library

---
+++ data/maps/haunted_house/library.lua
@@ -0,0 +1,11 @@
+local map = ...
+local game = map:get_game()
+
+map:register_event("on_started", function()
+  require("scripts/fx/map_settings/haunted_house_fx"):set_fx(map)
+
+end)
+
+function map:on_opening_transition_finished()
+
+end
