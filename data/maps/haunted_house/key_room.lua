07f7385a628a6938c9d1cbd1d9fb9b5853b6e43e
Max Mraz <max.mraz.11@gmail.com>
2020-10-21 13:24:06 -0400

    trap you in HH key room to prevent lack of monsters existing to open door by killing

--- data/maps/haunted_house/key_room.lua
+++ data/maps/haunted_house/key_room.lua
@@ -5,40 +5,48 @@ map:register_event("on_started", function()
   require("scripts/fx/map_settings/haunted_house_fx"):set_fx(map)
 
 
-for enemy in map:get_entities("group_a_enemy") do
-  function enemy:on_dead()
-    if not map:has_entities("group_a_enemy") then
-      map:focus_on(door_a, function()
-        for door in map:get_entities("door_a") do
-            door:set_enabled(false)
-        end
-      end)
+  for enemy in map:get_entities("group_a_enemy") do
+    function enemy:on_dead()
+      if not map:has_entities("group_a_enemy") then
+        map:focus_on(door_a, function()
+          for door in map:get_entities("door_a") do
+              door:set_enabled(false)
+          end
+        end)
+      end
     end
   end
-end
 
-for enemy in map:get_entities("group_b_enemy") do
-  function enemy:on_dead()
-    if not map:has_entities("group_b_enemy") then
-      map:focus_on(door_b, function()
-        for door in map:get_entities("door_b") do
-            door:set_enabled(false)
-        end
-      end)
+  for enemy in map:get_entities("group_b_enemy") do
+    function enemy:on_dead()
+      if not map:has_entities("group_b_enemy") then
+        map:focus_on(door_b, function()
+          for door in map:get_entities("door_b") do
+              door:set_enabled(false)
+          end
+        end)
+      end
     end
   end
-end
 
-for enemy in map:get_entities("group_c_enemy") do
-  function enemy:on_dead()
-    if not map:has_entities("group_c_enemy") then
-      map:focus_on(door_c, function()
-        for door in map:get_entities("door_c") do
-            door:set_enabled(false)
-        end
-      end)
+  for enemy in map:get_entities("group_c_enemy") do
+    function enemy:on_dead()
+      if not map:has_entities("group_c_enemy") then
+        map:focus_on(door_c, function()
+          for door in map:get_entities("door_c") do
+              door:set_enabled(false)
+          end
+        end)
+      end
     end
   end
-end
 
 end)
+
+for sensor in map:get_entities("trap_you_sensor") do
+  function sensor:on_activated()
+    for e in map:get_entities("door_c_trap") do
+      e:set_enabled(true)
+    end
+  end
+end
